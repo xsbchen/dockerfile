56afe6dd48384b6ce4e1e657a6eff1b24e8f66b6
xsbchen <xsbchen@qq.com>
2019-03-24 23:11:24 +0800

    save downloaded source to /tmp

--- asuswrt-merlin.ng-build/download_merlin.sh
+++ asuswrt-merlin.ng-build/download_merlin.sh
@@ -2,9 +2,9 @@
 
 ASUSWRT_MERLIN_NG_VERSION=384.9
 
-wget https://github.com/RMerl/asuswrt-merlin.ng/archive/${ASUSWRT_MERLIN_NG_VERSION}.tar.gz
+wget -P /tmp https://github.com/RMerl/asuswrt-merlin.ng/archive/${ASUSWRT_MERLIN_NG_VERSION}.tar.gz
 
-bsdtar -zxf ${ASUSWRT_MERLIN_NG_VERSION}.tar.gz
-rm ${ASUSWRT_MERLIN_NG_VERSION}.tar.gz
+bsdtar -zxf /tmp/${ASUSWRT_MERLIN_NG_VERSION}.tar.gz -C /tmp
+rm /tmp/${ASUSWRT_MERLIN_NG_VERSION}.tar.gz
 
-mv asuswrt-merlin.ng-${ASUSWRT_MERLIN_NG_VERSION} /home/asuswrt-merlin.ng
+mv /tmp/asuswrt-merlin.ng-${ASUSWRT_MERLIN_NG_VERSION} /home/asuswrt-merlin.ng
